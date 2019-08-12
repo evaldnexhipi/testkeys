7be067a2cc0296e0b1d35560cfe1915c34399d71
Islajd Meco <47759158+islajd@users.noreply.github.com>
2019-08-12 17:03:13 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,4 +0,0 @@
-gcloud compute instances create instance-1 --zone=europe-west4-a --machine-type=custom-1-3072 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=849744354128-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --image=ubuntu-1804-bionic-v20190722a --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=instance-1
-
-gcloud compute firewall-rules create test --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:443,tcp:80 --source-ranges=0.0.0.0/0
-
